3d69d6df6209131eb8a474623eb6a0fe00578ef7
ldw0123 <ldw0123@gmail.com>
2023-12-27 15:48:39 +0900

    socket room 구현

--- react-client/src/components/ClientChatting.js
+++ react-client/src/components/ClientChatting.js
@@ -1,4 +1,4 @@
-import './styles/chat.css';
+import '../styles/chat.css';
 import { useCallback, useMemo, useEffect, useState } from 'react';
 import Chat from './Chat';
 import Notice from './Notice';
@@ -6,20 +6,23 @@ import io from 'socket.io-client';
 
 const socket = io.connect('http://localhost:8000', { autoConnect: false });
 
-export default function Chatting3() {
+export default function ClientChatting() {
   const [msgInput, setMsgInput] = useState('');
   const [userIdInput, setUserIdInput] = useState('');
-  const [chatList, setChatList] = useState([]);
   const [userId, setUserId] = useState(null);
+  const [chatList, setChatList] = useState([]);
   const [userList, setUserList] = useState({});
-  const [dmTo, setDmTo] = useState('all');
+  const [frontList, setFrontList] = useState([]);
+  const [backList, setBackList] = useState([]);
+  const [fullList, setFullList] = useState([]);
+  const [dmTo, setDmTo] = useState('all'); // DM 기능
+  const [roomId, setRoomId] = useState('FRONTEND');
 
   const initSocketConnect = () => {
     console.log('connected', socket.connected);
     if (!socket.connected) socket.connect();
   };
 
-  // [실습 3-2] 닉네임 중복 방지
   // mount 시점에
   useEffect(() => {
     // initSocketConnect();
@@ -28,7 +31,7 @@ export default function Chatting3() {
       alert(res.msg);
     });
 
-    // 선수 입장
+    // 유저 입장
     socket.on('entrySuccess', (res) => {
       setUserId(res.userId);
     });
@@ -37,9 +40,20 @@ export default function Chatting3() {
     socket.on('userList', (res) => {
       setUserList(res);
     });
+
+    socket.on('updateFrontList', (res) => {
+      setFrontList(res);
+    });
+
+    socket.on('updateBackList', (res) => {
+      setBackList(res);
+    });
+
+    socket.on('updateFullList', (res) => {
+      setFullList(res);
+    });
   }, []);
 
-  // [실습 5]
   // useMemo: 값을 메모라이징한다
   // 의존성 배열에 있는 값이 update될 때마다 연산을 실행한다
   const userListOptions = useMemo(() => {
@@ -61,11 +75,28 @@ export default function Chatting3() {
       );
     }
     return options;
-  }, [userList]);
+  }, [userList, frontList, backList]);
 
-  // [실습 4, 5]
-  // Chatting3 컴포넌트가 리로딩이 되면 위의 코드를 쭉 읽어들인다
-  // useCallback: 함수를 메모라이징한다. 뒤에 있는 의존성 배열에 있는 값이 업데이트 될 때만 함수를 다시 선언한다. 그렇지 않은 경우, 기존에 있던 함수를 계속 사용한다.
+  const userListDivs = useMemo(() => {
+    let chooseList;
+    if (roomId === 'FRONTEND') chooseList = frontList;
+    else if (roomId === 'BACKEND') chooseList = backList;
+    else chooseList = fullList;
+
+    const divs = [];
+    for (const key in chooseList) {
+      if (chooseList[key] === userId) continue;
+      divs.push(
+        <div key={key} onClick={() => userChat(chooseList, key)}>
+          <hr />
+          <p className="user">👤 {chooseList[key]}</p>
+        </div>
+      );
+    }
+    return divs;
+  }, [userList, frontList, backList, fullList]);
+
+  // useCallback: 함수를 메모라이징한다. 뒤에 있는 의존성 배열에 있는 값이 업데이트 될 때만 함수를 다시 선언한다. 그렇지 않은 경우, 기존에 있던 함수를 계속 사용한다
   const addChatList = useCallback(
     (res) => {
       // 서버에서 송신한 userId와 내 userId가 같다면 type의 값은 my, 다르면 other
@@ -75,7 +106,6 @@ export default function Chatting3() {
       const content = `${res.dm ? '(속닥속닥) ' : ''} ${res.userId}: ${
         res.msg
       }`;
-
       const newChatList = [...chatList, { type: type, content: content }];
       setChatList(newChatList);
     },
@@ -93,71 +123,115 @@ export default function Chatting3() {
       const newChatList = [...chatList, { type: 'notice', content: res.msg }];
       setChatList(newChatList);
     };
-
     socket.on('notice', notice);
     return () => socket.off('notice', notice);
   }, [chatList]);
 
-  // [실습 4, 5]
   const sendMsg = () => {
     // 메시지가 비어있지 않을 경우 송신
     if (msgInput !== '') {
-      socket.emit('sendMsg', { userId: userId, msg: msgInput, dm: dmTo });
+      socket.emit('sendMsg', {
+        userId: userId,
+        msg: msgInput,
+        dm: dmTo,
+        roomId: roomId,
+      });
       setMsgInput('');
     }
   };
 
+  const handleEnter = (e) => {
+    if (e.key === 'Enter') sendMsg();
+  };
+
+  const EntryhandleEnter = (e) => {
+    if (e.key === 'Enter') entryChat();
+  };
+
+  const handleRefreshClick = () => {
+    window.location.reload(true);
+  };
+
   const entryChat = () => {
     initSocketConnect();
-    socket.emit('entry', { userId: userIdInput });
-    // [실습 3-2] 바로 userId에 값을 할당하지 않고
+    socket.emit('entry', { userId: userIdInput, roomId: roomId });
+    // 바로 userId에 값을 할당하지 않고
     // setUserId(userIdInput); // success
   };
 
-  return (
-    <>
-      <h3>실습 4, 5번</h3>
-      <ul>
-        <li>채팅창 메시지 전송</li>
-        <li>DM 기능 구현</li>
-      </ul>
+  const userChat = (chooseList, key) => {
+    // 단체 채팅방에서 유저 클릭시 1:1 채팅
+    console.log(chooseList[key]);
+  };
 
+  return (
+    <div className="chatting">
+      <h2>SeSAC Chat</h2>
       {userId ? (
         <>
-          <div>{userId}님 환영합니다.</div>
-          <div className="chat-container">
-            {chatList.map((chat, i) => {
-              if (chat.type === 'notice') return <Notice key={i} chat={chat} />;
-              else return <Chat key={i} chat={chat} />;
-            })}
-          </div>
-          <div className="input-container">
-            {/* [실습 5] DM 기능 구현 */}
-            <select value={dmTo} onChange={(e) => setDmTo(e.target.value)}>
-              <option value="all">전체</option>
-              {userListOptions}
-            </select>
-
-            <input
-              type="text"
-              value={msgInput}
-              onChange={(e) => setMsgInput(e.target.value)}
-            />
-            <button onClick={sendMsg}>전송</button>
+          <div className="main">
+            <div className="chat-nav">
+              <button onClick={handleRefreshClick}>✖️</button>
+              <p>{roomId}</p>
+            </div>
+            <div className="main-container">
+              <div className="chat-userlist">
+                <p>대화 상대</p>
+                <hr />
+                <p className="user">👤 나</p>
+                {userListDivs}
+              </div>
+              <div className="chat">
+                <div className="chat-container">
+                  {chatList.map((chat, i) => {
+                    if (chat.type === 'notice')
+                      return <Notice key={i} chat={chat} />;
+                    else return <Chat key={i} chat={chat} />;
+                  })}
+                </div>
+                <div className="input-container">
+                  {/* DM 기능 구현 */}
+                  <select
+                    value={dmTo}
+                    onChange={(e) => setDmTo(e.target.value)}
+                  >
+                    <option value="all">전체</option>
+                    {userListOptions}
+                  </select>
+                  <input
+                    type="text"
+                    value={msgInput}
+                    onChange={(e) => setMsgInput(e.target.value)}
+                    onKeyDown={handleEnter}
+                  />
+                  <button onClick={sendMsg}>전송</button>
+                </div>
+              </div>
+            </div>
           </div>
         </>
       ) : (
         <>
-          <div className="input-container">
+          <div className="entry-container ">
             <input
               type="text"
+              placeholder="닉네임을 입력해주세요"
               value={userIdInput}
               onChange={(e) => setUserIdInput(e.target.value)}
+              onKeyDown={EntryhandleEnter}
             />
+            <br />
+            <div>채팅방 선택</div>
+            <select value={roomId} onChange={(e) => setRoomId(e.target.value)}>
+              <option value="FRONTEND">FRONTEND</option>
+              <option value="BACKEND">BACKEND</option>
+              <option value="FULLSTACK">FULLSTACK</option>
+            </select>
+            <br />
             <button onClick={entryChat}>입장</button>
           </div>
         </>
       )}
-    </>
+    </div>
   );
 }
